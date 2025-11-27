96b12361db7a95d173b63ad2a9af273996e6090a
Junaid Jameel <junaidjameel396@gmail.com>
2025-11-27 09:52:12 +0500

    e

---
+++ lib/sdfd.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpandedWidget extends StatelessWidget {
+  const ExpandedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
